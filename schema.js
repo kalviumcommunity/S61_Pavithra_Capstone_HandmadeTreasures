ce2a2a35748f59a88d40f023f4bb48db4f89ae08
Pavithra8805 <pavithra8805@gmail.com>
2024-05-09 17:20:25 +0000

    Added subcategory in Schema

--- schema.js
+++ schema.js
@@ -2,15 +2,19 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const productSchema = new Schema({
+    image: { type: String, required: true },
     name: { type: String, required: true },
     category: {
         type: String,
         enum: ["crafts", "jewellery", "pottery", "painting"],
         required: true,
     },
+    subcategory: {
+        type: String,
+        enum: ["Candles", "Soap", "PaperCrafts", "WoodenItems", "Weaving", "Necklace", "Earring", "Bangles", "Bracelet", "JewellerySet", "CanvasPainting", "PaperPainting", "OilPainting", "WatercolorPainting", "Mug", "Bowl", "Planter", "Vase"],
+    },
     description: { type: String, required: true },
-    price: { type: Number, required: true },
-    image: { type: String, required: true },
+    price: { type: Number, required: true }
 });
 
 const productModel = mongoose.model("Product", productSchema);
